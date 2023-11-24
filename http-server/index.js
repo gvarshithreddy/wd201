da14f0a1f3c9f495b9919d96ff8ef540d0b46355
Varshith <gvarshithreddy8@gmail.com>
2023-11-24 09:51:07 +0530

    Node App Created with Regsitration Form

---
+++ http-server/index.js
@@ -0,0 +1,53 @@
+const http = require('http')
+const fs = require('fs')
+
+let homeContent = ''
+let projectContent = ''
+let registrationContent = ''
+
+fs.readFile('home.html', (err, home) => {
+  if (err) {
+    console.error(err)
+  } else {
+    homeContent = home
+  }
+})
+
+fs.readFile('project.html', (err, project) => {
+  if (err) {
+    console.error(err)
+  } else {
+    projectContent = project
+  }
+})
+
+fs.readFile('registration.html', (err, registration) => {
+  if (err) {
+    console.error(err)
+  } else {
+    registrationContent = registration
+    // console.log(registrationContent.toString())
+  }
+})
+
+http
+  .createServer((req, res) => {
+    const url = req.url
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    switch (url) {
+      case '/project':
+        res.write(projectContent)
+        res.end()
+        break
+      case '/registration':
+        res.write(registrationContent)
+        res.end()
+        break
+      default:
+        res.write(homeContent)
+        res.end()
+        break
+    }
+    res.end()
+  })
+  .listen(3000)
